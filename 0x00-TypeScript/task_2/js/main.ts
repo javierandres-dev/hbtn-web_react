08230e79196de898c27153ce91e0343d79aad85e
Javier Andrés <55107141+JA-programmer@users.noreply.github.com>
2021-02-08 12:56:58 -0500

    Task 5

---
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -0,0 +1,30 @@
+// 5. Advanced types Part 1
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+class Director implements DirectorInterface {
+  workFromHome = () => 'Working from home';
+  getCoffeeBreak = () => 'Getting a coffee break';
+  workDirectorTasks = () => 'Getting to director tasks';
+}
+
+class Teacher implements TeacherInterface {
+  workFromHome = () => 'Cannot work from home';
+  getCoffeeBreak = () => 'Cannot have a break';
+  workTeacherTasks = () => 'Getting to work';
+}
+
+const createEmployee = (salary: number | string) => Number(salary) < 500 ? "Teacher" : "Director"
+
+console.log(createEmployee(200));
+console.log(createEmployee(1000));
+console.log(createEmployee('$500'));
