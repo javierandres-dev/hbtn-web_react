76737616302821c5c23dbd766ad39ee4ac29e78c
Javier Andrés <55107141+JA-programmer@users.noreply.github.com>
2021-02-08 11:59:13 -0500

    Task 1

---
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -0,0 +1,20 @@
+'use strict';
+// 1. Let's build a Teacher interface
+interface Teacher {
+  readonly firstName: string,
+  readonly lastName: string,
+  fullTimeEmployee: boolean,
+  yearsOfExperience?: number,
+  location: string,
+  [key: string]: any,
+};
+
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher3);
