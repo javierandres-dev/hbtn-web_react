7644e15f93c8532862cf716b525363a7771fc9a3
Javier Andrés <55107141+JA-programmer@users.noreply.github.com>
2021-02-09 13:15:30 -0500

    Task 4

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -8,7 +8,7 @@ interface Teacher {
   location: string,
   [propName: string]: any;
 };
-/*
+
 const teacher3: Teacher = {
   firstName: 'John',
   fullTimeEmployee: false,
@@ -16,13 +16,13 @@ const teacher3: Teacher = {
   location: 'London',
   contract: false,
 };
-console.log(teacher3);
-*/
+//console.log(teacher3);
+
 // 2. Extending the Teacher class
 interface Directors extends Teacher {
   numberOfReports: number,
 };
-/*
+
 const director1: Directors = {
   firstName: 'John',
   lastName: 'Doe',
@@ -30,8 +30,8 @@ const director1: Directors = {
   fullTimeEmployee: true,
   numberOfReports: 17,
 };
-console.log(director1);
-*/
+//console.log(director1);
+
 // 3. Printing teachers
 interface printTeacherFunction {
   (firstName: string, lastName: string): string;
@@ -39,8 +39,10 @@ interface printTeacherFunction {
 
 export const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string =>`${firstName.charAt(0)}. ${lastName}`;
 
+//console.log(printTeacher("John", "Doe"));
+
 // 4. Writing a class
-interface StudentClassConstructor {
+interface StudentConstructor {
     new (firstName: string, lastName: string): StudentClassInterface;
 }
 
@@ -66,5 +68,3 @@ export class StudentClass implements StudentClassInterface {
         return this.firstName;
     }
 }
-
-console.log(printTeacher("John", "Doe"));
