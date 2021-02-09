88a37b0c078f041ca295272942d12341d2013c89
Javier Andrés <55107141+JA-programmer@users.noreply.github.com>
2021-02-09 13:17:42 -0500

    Task 4

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -43,28 +43,28 @@ export const printTeacher: printTeacherFunction = (firstName: string, lastName: 
 
 // 4. Writing a class
 interface StudentConstructor {
-    new (firstName: string, lastName: string): StudentClassInterface;
+  new (firstName: string, lastName: string): StudentClassInterface;
 }
 
 interface StudentClassInterface {
-    workOnHomework(): string;
-    displayName(): string;
+  workOnHomework(): string;
+  displayName(): string;
 }
 
 export class StudentClass implements StudentClassInterface {
-    firstName: string;
-    lastName: string;
+  firstName: string;
+  lastName: string;
 
-    constructor(firstName: string, lastName: string) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
 
-    workOnHomework(): string {
-        return "Currently working";
-    }
+  workOnHomework(): string {
+    return "Currently working";
+  }
 
-    displayName(): string {
-        return this.firstName;
-    }
+  displayName(): string {
+    return this.firstName;
+  }
 }
