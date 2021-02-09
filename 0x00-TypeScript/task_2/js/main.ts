eec1189691c70bf6f6d32919e2ddf75a9029a562
Javier Andrés <55107141+JA-programmer@users.noreply.github.com>
2021-02-09 13:30:03 -0500

    Task 5

--- 0x00-TypeScript/task_2/js/main.ts
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -11,26 +11,27 @@ interface TeacherInterface {
   workTeacherTasks(): string;
 }
 
-class Director implements DirectorInterface {
+export class Director implements DirectorInterface {
   workFromHome = () => 'Working from home';
   getCoffeeBreak = () => 'Getting a coffee break';
   workDirectorTasks = () => 'Getting to director tasks';
 }
 
-class Teacher implements TeacherInterface {
+export class Teacher implements TeacherInterface {
   workFromHome = () => 'Cannot work from home';
   getCoffeeBreak = () => 'Cannot have a break';
   workTeacherTasks = () => 'Getting to work';
 }
 
-const createEmployee = (salary: number | string): Teacher | Director => Number(salary) < 500 ? new Teacher() : new Director()
+export const createEmployee = (salary: number | string): Teacher | Director => Number(salary) < 500 ? new Teacher() : new Director()
 
-//console.log(createEmployee(200));
-//console.log(createEmployee(1000));
-//console.log(createEmployee('$500'));
+console.log(createEmployee(200));
+console.log(createEmployee(1000));
+console.log(createEmployee('$500'));
 
 // 6. Creating functions specific to employees
 //const isDirector = (employee: Director | Teacher): boolean => employee.workFromHome() === 'Working from home';
+/*
 const isDirector = (employee: TeacherInterface | DirectorInterface): employee is Director => (employee as Director).workDirectorTasks !== undefined;
 
 const executeWork = (employee: TeacherInterface | DirectorInterface): void => {
@@ -41,3 +42,4 @@ const executeWork = (employee: TeacherInterface | DirectorInterface): void => {
 
 executeWork(createEmployee(200));
 executeWork(createEmployee(1000));
+*/
