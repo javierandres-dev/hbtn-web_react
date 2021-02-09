e735624a5256a88a359aaa5ed7f91b2c47e6a891
Javier Andrés <55107141+JA-programmer@users.noreply.github.com>
2021-02-09 11:27:10 -0500

    Task 0

--- 0x00-TypeScript/task_0/js/main.ts
+++ 0x00-TypeScript/task_0/js/main.ts
@@ -23,16 +23,26 @@ const secondSudent: Student = {
 
 const studentsList: Array<Student> = [ firstSudent, secondSudent ];
 
-const $body: HTMLBodyElement  = document.querySelector('body');
-const $table: HTMLTableElement = document.createElement('table');
+const $body: HTMLBodyElement = document.querySelector("body");
+const $table: HTMLTableElement = document.createElement("table");
+const $thead: HTMLTableSectionElement = document.createElement("thead");
+const $tbody: HTMLTableSectionElement = document.createElement("tbody");
+const $rowHead: HTMLTableRowElement = $thead.insertRow(0);
+const $cell1Head: HTMLTableCellElement = $rowHead.insertCell(0);
+const $cell2Head: HTMLTableCellElement = $rowHead.insertCell(1);
+
+$cell1Head.textContent = "firstName";
+$cell2Head.textContent = "location";
+$table.append($thead);
 
 studentsList.forEach((student: Student) => {
-  const $row: HTMLTableRowElement = $table.insertRow()
-  const $name: HTMLTableDataCellElement = document.createElement('td');
-  const $location: HTMLTableDataCellElement = document.createElement('td');
-  $name.textContent = student.firstName;
-  $location.textContent = student.location;
-  $row.appendChild($name);
-  $row.appendChild($location);
+  const $row: HTMLTableRowElement = $tbody.insertRow(0);
+  const $cell1: HTMLTableCellElement = $row.insertCell(0);
+  const $cell2: HTMLTableCellElement = $row.insertCell(1);
+
+  $cell1.textContent = student.firstName;
+  $cell2.textContent = student.location;
 });
-$body.appendChild($table);
+
+$table.append($tbody);
+$body.append($table);
