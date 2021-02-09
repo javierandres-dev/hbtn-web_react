0629a0719b443e23862baf0d75fb4d515417936b
Javier Andrés <55107141+JA-programmer@users.noreply.github.com>
2021-02-09 15:06:37 -0500

    Task 7

--- 0x00-TypeScript/task_2/js/main.ts
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -47,7 +47,7 @@ export function executeWork(employee: DirectorInterface | TeacherInterface): str
 // 7. String literal types
 type Subjects = "Math" | "History";
 
-export function teachClass(todayClass:Subjects): string {
+export function teachClass(todayClass: Subjects): string {
   if (todayClass === "Math") {
     return "Teaching Math";
   } else if (todayClass === "History") {
