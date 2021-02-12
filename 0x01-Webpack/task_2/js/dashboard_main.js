baf20ad7069375640600c9055c916f92898e5230
Javier Andrés <55107141+JA-programmer@users.noreply.github.com>
2021-02-12 17:57:39 -0500

    Task 2

--- 0x01-Webpack/task_2/js/dashboard_main.js
+++ 0x01-Webpack/task_2/js/dashboard_main.js
@@ -1,18 +1,18 @@
-"use strict";
-import $ from "jquery";
-import _ from "lodash";
+'use strict';
+import $ from 'jquery';
+import _ from 'lodash';
 
-$("body").append("<p>Holberton Dashboard</p>");
-$("body").append("<p>Dashboard data for the students</p>");
-$("body").append("<button>Click here to get started</button>");
-$("body").append('<p id="count"></p>');
-$("body").append("<p>Copyright - Holberton School</p>");
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
 
 const updateCounter = () => {
-  let times = $("#count").html() || 0;
-  $("button").on("click", () => {
+  let times = $('#count').html() || 0;
+  $('button').on('click', () => {
     times++;
-    $("#count").html(`${times} clicks on the button`);
+    $('#count').html(`${times} clicks on the button`);
   });
 };
 
