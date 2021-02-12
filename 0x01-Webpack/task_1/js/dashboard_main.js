5d5109c8fa68defbbe8caf847396709b4ce57489
Javier Andrés <55107141+JA-programmer@users.noreply.github.com>
2021-02-12 17:48:16 -0500

    Task 1

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -5,7 +5,7 @@ import _ from 'lodash';
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
-$('body').append('<p id='count'></p>');
+$('body').append('<p id="count"></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
 
 const updateCounter = () => {
