c364633bd93afa5dc83e1fd7a3eb94ac4626b561
Javier Andrés <55107141+JA-programmer@users.noreply.github.com>
2021-02-12 20:49:42 -0500

    Task 3

--- 0x01-Webpack/task_3/webpack.config.js
+++ 0x01-Webpack/task_3/webpack.config.js
@@ -1,15 +1,33 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     mode: 'development',
     entry : path.resolve(__dirname, 'js/dashboard_main.js'),
     output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: 'bundle.js'
+      path: path.resolve(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+      },
     },
     performance: {
-    maxAssetSize: 1000000,
-  },
+      maxAssetSize: 1000000,
+    },
+    devServer: {
+      contentBase: path.join(__dirname, './public'),
+      compress: true,
+      port: 8564,
+    },
+    plugins: [
+      new HTMLWebpackPlugin({
+        filename: 'public/index.html'
+      }),
+      new CleanWebpackPlugin()
+    ],
     module: {
     rules: [
       {
