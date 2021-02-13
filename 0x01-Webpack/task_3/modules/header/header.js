16708174b8ad307c0a2fe2d5b64b2e50fde59360
Javier Andrés <55107141+JA-programmer@users.noreply.github.com>
2021-02-12 20:53:11 -0500

    Task 3

--- 0x01-Webpack/task_3/modules/header/header.js
+++ 0x01-Webpack/task_3/modules/header/header.js
@@ -3,7 +3,6 @@ const $ = require('jquery');
 import './header.css';
 
 $('body').append('<header></header>');
-$('header').append('<div id="logo"></div>',
-                               '<h1>Holberton Dashboard</h1>');
+$('header').append('<div id="logo"></div>', '<h1>Holberton Dashboard</h1>');
 
 console.log('Init header');
