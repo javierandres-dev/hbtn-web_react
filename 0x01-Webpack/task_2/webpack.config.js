8b0f32ce10f5fed305a6f43df5a332898e600177
Javier Andrés <55107141+JA-programmer@users.noreply.github.com>
2021-02-12 19:29:55 -0500

    Task 2

--- 0x01-Webpack/task_2/webpack.config.js
+++ 0x01-Webpack/task_2/webpack.config.js
@@ -1,34 +1,27 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'production',
-  entry: './js/dashboard_main.js',
-  performance: {
+    mode: 'production',
+    entry : path.resolve(__dirname, 'js/dashboard_main.js'),
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    performance: {
     maxAssetSize: 1000000,
   },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public'),
-  },
-  module: {
+    module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(gif|png|jpe?g|svg)$/i,
-        use: [
-          'file-loader',
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              bypassOnDebug: true,
-              disable: true,
-            },
-          },
-        ],
-      },
-    ],
-  },
-};
+        test: /\.jpg$/,
+        use: {
+          loader: 'file-loader'
+        }
+      }
+    ]
+  }
+}
