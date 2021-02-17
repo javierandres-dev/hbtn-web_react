cd7c38fcce4d133d797bd78adcae64beab0aab56
Javier Andrés <55107141+JA-programmer@users.noreply.github.com>
2021-02-17 09:41:24 -0500

    Task 2

--- 0x02-react_intro/task_2/dashboard/src/App.js
+++ 0x02-react_intro/task_2/dashboard/src/App.js
@@ -13,7 +13,7 @@ function App() {
         <p>Login to access the full dashboard</p>
         <label htmlFor='email'>Email</label>
         <input type='email' name='email' id='email' />
-        <label htmlFor='password'>Email</label>
+        <label htmlFor='password'>Password</label>
         <input type='password' name='password' id='password' />
         <button type='button'>OK</button>
       </main>
