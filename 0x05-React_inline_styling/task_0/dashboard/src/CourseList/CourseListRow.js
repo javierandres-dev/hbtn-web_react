29efd863d8524cefafec994259977a8619785fd0
Javier Andrés <55107141+JA-programmer@users.noreply.github.com>
2021-03-03 10:59:40 -0500

    Task 0

--- 0x05-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.js
+++ 0x05-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.js
@@ -1,13 +1,14 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 
-const bgColor1 = { backgroundColor: '#f5f5f5ab' };
-const bgColor2 = { backgroundColor: '#deb5b545' };
-
 const CourseListRow = ({ isHeader, textFirstCell, textSecondCell }) => {
+  const bgColor1 = { backgroundColor: '#f5f5f5ab' };
+  const bgColor2 = { backgroundColor: '#deb5b545' };
+  let bgColor = undefined;
   let content = undefined;
 
   if (isHeader === true) {
+    bgColor = bgColor2;
     if (textSecondCell === null) {
       content = <th colSpan='2'>{textFirstCell}</th>;
     } else {
@@ -20,6 +21,7 @@ const CourseListRow = ({ isHeader, textFirstCell, textSecondCell }) => {
     }
   }
   if (isHeader === false) {
+    bgColor = bgColor1;
     content = (
       <Fragment>
         <td>{textFirstCell}</td>
@@ -27,12 +29,7 @@ const CourseListRow = ({ isHeader, textFirstCell, textSecondCell }) => {
       </Fragment>
     );
   }
-  let bgColor = undefined;
-  if (isHeader) {
-    bgColor = bgColor2;
-  } else {
-    bgColor = bgColor1;
-  }
+
   return <tr style={bgColor}>{content}</tr>;
 };
 
