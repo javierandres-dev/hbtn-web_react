f24dbc3eac921b2c2f280d45c182d37b04953cd4
Javier Andrés <55107141+JA-programmer@users.noreply.github.com>
2021-03-15 14:07:45 -0500

    Task 8

--- 0x07-React_Immutable/8-seq.js
+++ 0x07-React_Immutable/8-seq.js
@@ -3,23 +3,24 @@ import { Seq } from 'immutable';
 const printBestStudents = (object) => {
   const seq = Seq(object);
 
-  const students = seq
-    .filter((value) => value.score > 70)
-    .map((key) =>
-      Map(key).withMutations((elem) => {
-        elem
-          .set(
-            'firstName',
-            key.firstName.charAt(0).toUpperCase() + key.firstName.slice(1)
-          )
-          .set(
-            'lastName',
-            key.lastName.charAt(0).toUpperCase() + key.lastName.slice(1)
-          );
-      })
-    )
-    .toJS();
-  console.log(students);
+  console.log(
+    seq
+      .filter((value) => value.score > 70)
+      .map((key) =>
+        Map(key).withMutations((elem) => {
+          elem
+            .set(
+              'firstName',
+              key.firstName.charAt(0).toUpperCase() + key.firstName.slice(1)
+            )
+            .set(
+              'lastName',
+              key.lastName.charAt(0).toUpperCase() + key.lastName.slice(1)
+            );
+        })
+      )
+      .toJS()
+  );
 };
 
 export default printBestStudents;
