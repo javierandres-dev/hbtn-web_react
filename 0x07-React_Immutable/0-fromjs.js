1f77c812651aa20cd9218d5e6fa7789c89660a48
Javier Andrés <55107141+JA-programmer@users.noreply.github.com>
2021-03-15 12:59:35 -0500

    Task 0

--- 0x07-React_Immutable/0-fromjs.js
+++ 0x07-React_Immutable/0-fromjs.js
@@ -1,3 +1,4 @@
 import { fromJS } from 'immutable';
-const getImmutableObject = (object) => fromJS(object);
-export default getImmutableObject;
+export default function getImmutableObject(object) {
+  return fromJS(object);
+}
